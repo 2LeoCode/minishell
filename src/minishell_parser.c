4451040c2f03f9a9ec94a197d283b391c706cee5
Leo SUARDI <lsuardi@e2r11p14.42.fr>
2021-06-05 14:01:25 +0200

    lol

--- src/minishell_parser.c
+++ src/minishell_parser.c
@@ -12,3 +12,53 @@
 
 #include <minishell.h>
 
+/*size_t	cmd_count(char **tokens)
+{
+	size_t	count;
+
+	count = 1;
+	while (*tokens)
+	{
+		if (**tokens ==  "|" || (**tokens == ";" && tokens[1]))
+			count++;
+		tokens++;
+	}
+	return (count);
+}
+
+t_cmd	**parser(char **tokens)
+{
+	const size_t	count = cmd_count(tokens);
+	size_t			ac;
+	t_cmd			**cmd_arr;
+	int				i;
+	int				j;
+	bool			pipe;
+
+	if (gb_alloc(&cmd_arr, sizeof(t_cmd *) * (count + 1)))
+		return (NULL);
+	cmd_arr[count] = NULL;
+	i = -1;
+	while (++i < count)
+	{
+		ac = arg_count(tokens);
+		cmd_arr[i]->argc = ac;
+		if (gb_alloc(tokens + i, sizeof(t_cmd) + ac * sizeof(char *)))
+			return (NULL);
+		j = 0;
+		while (**tokens != "|" && **tokens != ";")
+		{
+			if (**tokens == ">" || **tokens == "<")
+			{
+				REDIRECTION;
+				free(*tokens);
+				*tokens = NULL;
+			}
+			else
+				cmd_arr[i]->argv[j] = *tokens;
+		}
+		if (**tokens == "|")
+			cmd_arr->pipe = true;
+	}
+}
+*/
