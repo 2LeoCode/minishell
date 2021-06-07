01f1d0049dd7f1534a986ed06a1865b99a5a1830
martinovich89 <dsinemoi1mouton@gmail.com>
2021-06-07 14:23:59 +0200

    added pseudo checker

--- src/parsing/lexer.c
+++ src/parsing/lexer.c
@@ -16,24 +16,87 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-size_t	ft_skip_space(char *str)
+int     ft_isspace(char c)
 {
-	size_t i;
+    return (c == ' ' || c == '\f' || c == '\r' || c == '\n' || c == '\v' || c == '\t');
+}
 
-	i = 0;
-	while (str[i] && str[i] == ' ')
-		i++;
-	return (i);
+size_t  ft_skip_whitespace(char *str)
+{
+    size_t  i;  
+
+    i = 0;
+    while (str[i] && ft_isspace(str[i]))
+        i++;
+    return (i);
+}
+
+int     check_first_character(char *str)
+{
+    size_t  i;  
+
+    i = 0;
+    ft_skip_whitespace(str);
+    if (str[i] == ';' || str[i] == '|')
+        return (1);
+    return (0);
+}
+
+int     check_last_character(char *str)
+{
+    size_t  i;
+
+    i = 0;
+    while (str[i])
+        i++;
+    i--;
+    while (i > 0 && ft_isspace(str[i]))
+        i--;
+    //  printf("%zu %c\n", i, str[i]);
+    if (str[i] == '|')
+        return (2);
+    return (0);
+}
+
+int     check_operator_excess(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i])
+    {
+        if (strchr("<>|;", str[i]))
+        {
+            printf("i = %i\n", i);
+            if (str[i] == '>' && str[i + 1] == '>')
+                i++;
+            i++;
+            i += ft_skip_whitespace(str + i);
+            printf("i = %i\n", i);
+            printf("str[i] = %c\n", str[i]);
+            if (strchr("<>|;", str[i]))
+            {
+                write(1, "kek\n", 4);
+                return (3);
+            }
+        }
+        i++;
+    }
+    return (0);
 }
 
 int		check_syntax(char *str)
 {
-	
+	if (check_first_character(str) || check_last_character(str)
+	|| check_operator_excess(str))
+		return (1);
+	return (0);
+//		MINISHELL_FAILURE;
 }
 
 size_t	skip_quote_token(char *str)
 {
-	size_t i;
+	size_t	i;
 
 	i = 0;
 	if (str[i] == '"')
@@ -57,7 +120,7 @@ size_t	skip_quote_token(char *str)
 
 size_t	get_next_token(char *str)
 {
-	size_t i;
+	size_t	i;
 
 	i = 0;
 	if (str[i] == '<' || str[i] == '>')
@@ -79,7 +142,7 @@ size_t	get_next_token(char *str)
 				i++;
 		}
 	}
-	i += ft_skip_space(str + i);
+	i += ft_skip_whitespace(str + i);
 	return (i);
 }
 
@@ -90,7 +153,7 @@ size_t	token_cnt(char *str)
 	size_t	cnt;
 
 	cnt = 0;
-	i = ft_skip_space(str);
+	i = ft_skip_whitespace(str);
 	sentinel = str + i;
 	while (str[i])
 	{
@@ -133,7 +196,7 @@ char	**tkntab_alloc(char *str, size_t cnt)
 	tab = malloc(sizeof(char *) * cnt + 1);  // MALLOC !
 //	if (tab == NULL)
 //		MINISHELL_FAILURE;
-	i = ft_skip_space(str);
+	i = ft_skip_whitespace(str);
 	j = 0;
 	while (str[i])
 	{
@@ -156,7 +219,7 @@ char **lexer(char *input_str)
 	return (tkntab_alloc(input_str, cnt));
 }
 
-
+/*
 int main(int argc, char **argv)
 {
 	char	**cmd_tab;
@@ -174,4 +237,14 @@ int main(int argc, char **argv)
 		i++;
 	}
 	return (0);
+}
+*/
+
+int main(void)
+{
+	int n;
+	char *str = "bonjour |     ";
+
+	n = check_last_character(str);
+	printf("%i\n", n);
 }
---
+++ src/parsing/syntax_checker.c
@@ -0,0 +1,110 @@
+//#include "minishell.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int		ft_isspace(char c)
+{
+	return (c == ' ' || c == '\f' || c == '\r' || c == '\n' || c == '\v' || c == '\t');
+}
+
+size_t  ft_skip_whitespace(char *str)
+{
+	size_t  i;
+
+	i = 0;
+	while (str[i] && ft_isspace(str[i]))
+		i++;
+	return (i);
+}
+
+int     check_first_character(char *str)
+{
+	size_t  i;  
+
+	i = 0;
+	ft_skip_whitespace(str);
+	if (str[i] == ';' || str[i] == '|')
+		return (1);
+	return (0);
+}
+
+int     check_last_character(char *str)
+{
+	size_t  i;  
+
+	i = 0;
+	while (str[i])
+		i++;
+	i--;
+	while (i > 0 && ft_isspace(str[i]))
+		i--;
+	//	printf("%zu %c\n", i, str[i]);
+	if (str[i] == '|')
+		return (2);
+	return (0);
+}
+
+int     check_operator_excess(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (strchr("<>|;", str[i]))
+		{
+			printf("i = %i\n", i);
+			if (str[i] == '>' && str[i + 1] == '>')
+				i++;
+			i++;
+			i += ft_skip_whitespace(str + i);
+			printf("i = %i\n", i);
+			printf("str[i] = %c\n", str[i]);
+			if (strchr("<>|;", str[i]))
+			{
+				write(1, "kek\n", 4);
+				return (3);
+			}
+		}
+		i++;
+	}   
+	return (0);
+}
+
+/*
+   int     check_double_cmdseparator(char *str)
+   {
+   size_t  i;
+
+   i = 0;
+   while (str[i])
+   {
+   if (str[i] == '|')
+   {
+   i += ft_skip_whitespace(str);
+   if (str[i] == '|')
+   return (3);
+   }
+   if (str[i] == ';')
+   {
+   i += ft_skip_whitespace(str);
+   if (str[i] == ';')
+   return (3);
+   }
+   }
+   return (0);
+   }
+ */
+
+int main(int argc, char **argv)
+{
+	//	char *str = "echo bonjour | cat -e ;   \t\v|   \n  ";
+
+	(void)argc;
+	printf("check_last_character = %i\n", check_last_character(argv[1]));
+	printf("check_first_character = %i\n", check_first_character(argv[1]));
+	printf("check_operator_excess = %i\n", check_operator_excess(argv[1]));
+	return (0);
+}
